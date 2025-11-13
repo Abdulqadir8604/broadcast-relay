13c48a5e1c20d3ab9678b45372b057e5b2f19aa5
Abdulqadir <76737223+Abdulqadir8604@users.noreply.github.com>
2025-11-13 22:29:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import "../App.css";
 function Footer({theme}) {
     return (
         <footer>
-        <p className="footer_text_h3">Developed by AQ</p>
-            // <p className="footer_text_h3">Hinduja College</p>
-        // <p className="footer_text_p">Developed with ❤️ by Umaima & Fatema</p>
+            <p className="footer_text_h3">Developed by AQ</p>
+            {/* <p className="footer_text_h3">Hinduja College</p> */} {/* This paragraph is commented out, possibly to hide or temporarily disable displaying Hinduja College */}
+            {/* <p className="footer_text_p">Developed with ❤️ by Umaima & Fatema</p> */} {/* This paragraph is commented out, likely to hide or disable the credit line for Umaima & Fatema */}
         </footer>
     );
 }
