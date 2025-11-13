fac9765efddf1fe5ee20d33332c263b87160ef62
Abdulqadir <76737223+Abdulqadir8604@users.noreply.github.com>
2025-11-13 22:28:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,9 @@ import "../App.css";
 function Footer({theme}) {
     return (
         <footer>
-            <p className="footer_text_h3">Hinduja College</p>
-            <p className="footer_text_p">Developed with ❤️ by Umaima & Fatema</p>
+        <p className="footer_text_h3">Developed by AQ</p>
+            // <p className="footer_text_h3">Hinduja College</p>
+        // <p className="footer_text_p">Developed with ❤️ by Umaima & Fatema</p>
         </footer>
     );
 }
